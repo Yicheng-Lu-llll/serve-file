502582f7676fd93dfa6fd122c06f89cebc72b3f2
Yicheng-Lu-llll <51814063+Yicheng-Lu-llll@users.noreply.github.com>
2023-12-24 18:23:56 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from ray import serve
+import time
 
 @serve.deployment(num_replicas=1, ray_actor_options={"num_cpus": 0.2, "num_gpus": 0})
 class helloWorld:
@@ -8,6 +9,8 @@ class helloWorld:
     async def __call__(self, http_request) -> str:
         data = await http_request.json()
         name = data.get("name", "world")
+        sleep_duration = float(data.get("seconds", 120))
+        time.sleep(sleep_duration)
         return self.text + name
 
 deployment_graph = helloWorld.bind()
